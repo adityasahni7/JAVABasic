0615be08c830175c76b95b2b0622c948f29d7b57
Aditya Sahni <sahniaditya007@gmail.com>
2023-02-08 23:44:46 +0530

    java

---
+++ UserInput/userinput1.java
@@ -0,0 +1,10 @@
+package UserInput;
+
+public class userinput1 {
+    public static void main(String[] args) {
+        Scanner sc = new(System.in);
+        System.out.println("What is your name?");
+        scanner.nextLine();
+        System.out.println();
+    }
+}
