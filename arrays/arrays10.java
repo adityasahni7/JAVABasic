cc897e474482e338f646e44bcf1d5097360e6aae
Aditya Sahni <sahniaditya007@gmail.com>
2023-02-25 23:30:00 +0530

    java

--- arrays/arrays10.java
+++ arrays/arrays10.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class arrays10 {
     public static void main(String[] args) {
         int number[] = {1,2,3,4,5};
-        int copyOfNumbers[] = number;
+        int copyOfNumbers[] = Arrays.copyOf(number, number.length);
         Arrays.fill(number,0);
         System.out.println(Arrays.toString(number));
         System.out.println(Arrays.toString(copyOfNumbers));
