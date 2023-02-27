cf0e33d680aa81fbe574aea1111900c4615ed54f
Aditya Sahni <sahniaditya007@gmail.com>
2023-02-27 23:52:21 +0530

    java

--- arrays/arrays12.java
+++ arrays/arrays12.java
@@ -1,9 +1,9 @@
 package arrays;
 import java.util.*;
-public class arrays11 {
+public class arrays12 {
     public static void main(String[] args) {
         int numbers[] = {1,2,3,4,5};
-        int copyOfNumbers[] = Arrays.copyOf(numbers, numbers.length);
+        int copyOfNumbers[] = Arrays.copyOf(numbers, 2);
         System.out.println(Arrays.toString(numbers));
         System.out.println(Arrays.toString(copyOfNumbers));
     }
