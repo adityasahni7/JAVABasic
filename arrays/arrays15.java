09d499e2a0ea7150cb832da46d16909cb5d6b03d
Aditya Sahni <sahniaditya007@gmail.com>
2023-02-28 00:13:57 +0530

    java

---
+++ arrays/arrays15.java
@@ -0,0 +1,7 @@
+package arrays;
+
+public class arrays15 {
+    int numbers[] = {1,2,3,4,5};
+    int copyOfnumbers[] = Arrays.copyOf(numbers. numbers.length);
+    System.out.println(Arrays.toString(numbers, copyOfNumbers));
+}
