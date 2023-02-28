b5a137b8605de796eeb011270ebac2e069367e1e
Aditya Sahni <sahniaditya007@gmail.com>
2023-02-28 08:58:24 +0530

    java

---
+++ ForLoops/ForLoops.java
@@ -0,0 +1,9 @@
+package ForLoops;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        for(int number = 1; number<=10; number++){
+            System.out.println(number);
+        }
+    }
+}
