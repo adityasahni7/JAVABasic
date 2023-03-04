69f8167a05b7d04546351976e1028da463c0dbc1
Aditya Sahni <sahniaditya007@gmail.com>
2023-03-05 01:09:16 +0530

    java

---
+++ ArrayList/ArrayList.java
@@ -0,0 +1,13 @@
+package ArrayList;
+import java.util.*;
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList numbers = new ArrayList();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        System.out.println(numbers.toString());
+    }
+}
