92d2e27b5b8f8188b8bf3ea160cda79c7b393b5c
Aditya Sahni <sahniaditya007@gmail.com>
2023-03-05 03:54:18 +0530

    java

---
+++ HashMap/HashMap10.java
@@ -0,0 +1,19 @@
+package HashMap;
+import java.util.HashMap;
+public class HashMap10 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> examScores = new HashMap<String, Integer>();
+        examScores.put("Math", 75);
+        examScores.put("Sociology", 85);
+        examScores.put("English", 95);
+        examScores.put("History", 90);
+        examScores.put("Physics", 80);
+        examScores.put("Chemistry", 70);
+        examScores.put("Biology", 65);
+        examScores.put("Geography", 60);
+        examScores.forEach((subject, score) ->{
+            System.out.println(subject + " " + score);
+        });
+        System.out.println(examScores.isEmpty());
+    }
+}
