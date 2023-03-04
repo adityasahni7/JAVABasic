5508cfa7fba36da64ede257b8b58a55efb544c76
Aditya Sahni <sahniaditya007@gmail.com>
2023-03-05 00:21:45 +0530

    java

--- ForLoops/ForLoops6.java
+++ ForLoops/ForLoops6.java
@@ -3,8 +3,10 @@ package ForLoops;
 public class ForLoops6 {
     public static void main(String[] args) {
         int numbers[] = {1,2,3,4,5};
+        int sum = 0;
         for(int number : numbers){
-            System.out.println(number);
+            sum += number;
         }
+        System.out.println(sum);
     }
 }
---
+++ OtherLoops/WhileLoops.java
@@ -0,0 +1,12 @@
+package OtherLoops;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        int number = 5;
+        int multiplier = 1;
+        while(multiplier <= 10){
+            System.out.printf("%d X %d = %d \n", number, multiplier, number*multiplier);
+            multiplier++;
+        }
+    }
+}
