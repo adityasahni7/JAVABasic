1953a21d12712d164abdcc696624b6ba1420aa68
Aditya Sahni <sahniaditya007@gmail.com>
2023-03-05 17:00:14 +0530

    Java

--- OOPS/OOPS1/HelloWorld.java
+++ OOPS/OOPS1/HelloWorld.java
@@ -1,14 +1,12 @@
 package OOPS.OOPS1;
-import java.time.LocalDate;
 public class HelloWorld {
     public static void main(String[] args) {
-        User youngerUser = new User();
-        youngerUser.name = "John";
-        youngerUser.birthDay = LocalDate.of(1990, 1, 1);
-        User olderUser = new User();
-        olderUser.name = "Jane";
-        olderUser.birthDay = LocalDate.of(1980, 1, 1);
-        System.out.println(youngerUser.name + " is " + youngerUser.age() + " years old.");
-        System.out.println(olderUser.name + " is " + olderUser.age() + " years old.");
+        User User = new User("John","1990, 1, 1");
+        Book book = new Book();
+        book.title = "The Lord of the Rings";
+        book.author = "J.R.R. Tolkien";
+        User.borrow(book);
+        System.out.printf(User.name + " is " + User.age() + " years old. \n");
+        System.out.printf("%s has borrowed these books: %s \n", User.name, User.books.toString());
     }
 }
